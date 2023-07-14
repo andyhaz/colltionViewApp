3a2890a6da312377d76a5245dc1e072e0e873b6a
andrew <andyhaz1970@gmail.com>
2023-07-14 11:14:59 -0400

    Initial Commit

---
+++ colltionViewApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  colltionViewApp
+//
+//  Created by andyhaz on 7/14/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
